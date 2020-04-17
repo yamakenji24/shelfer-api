9447b58d7ae7338d9329b94f2805d4c935642c84
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-04-17 14:27:08 +0900

    routing

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/yamakenji24/shelfer-api/controllers"
+	"github.com/yamakenji24/shelfer-api/db"
+)
+
+func main() {
+	err := godotenv.Load(fmt.Sprintf("%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		fmt.Println(err)
+	}
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	router.POST("login", func(c *gin.Context) {
+		controllers.Login(c)
+	})
+	db.Connection()
+
+	router.Run()
+
+	db.Close()
+}
