fee612f5c31271f357889958cf7f42765a97f8da
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-04-17 14:25:56 +0900

    db用のモデルの生成と定義

---
+++ models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginParams struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
