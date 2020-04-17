cc3528dce278ae155d2ccc5786624c3967b4a48b
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-04-17 14:25:26 +0900

    DB関連の処理

---
+++ db/db.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/yamakenji24/shelfer-api/models"
+)
+
+var (
+	db  *gorm.DB
+	err error
+)
+
+func Connection() {
+	dbuser := os.Getenv("dbuser")
+	dbpass := os.Getenv("dbpass")
+	dbname := os.Getenv("dbname")
+	db, err = gorm.Open(
+		"postgres",
+		"user="+dbuser+" dbname="+dbname+" password="+dbpass+" sslmode=disable",
+	)
+	if err != nil {
+		panic(err)
+	}
+	autoMigration()
+}
+func GetDB() *gorm.DB {
+	return db
+}
+func Close() {
+	if err := db.Close(); err != nil {
+		panic(err)
+	}
+}
+func autoMigration() {
+	db.AutoMigrate(&models.User{})
+}
